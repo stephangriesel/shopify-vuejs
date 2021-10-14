a70bfd2294ec2e5b00d3bf75436bcd5c65f9639e
Stephan <sgriesel@gmail.com>
2021-10-14 06:19:57 +0200

    minicart logic

--- theme/assets/application.js
+++ theme/assets/application.js
@@ -1,1 +1,43 @@
-// Put your application javascript here
+const store = Vue.reactive({
+    state: {
+        cartState: []
+    },
+    getCart() {
+        axios.get('/cart.js')
+            .then(response => {
+                this.state.cartState.unshift(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+})
+
+miniCartState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMiniCart = {
+    openCart(){
+        miniCartState.hidden = !miniCartState.hidden
+        menuState.hidden = true
+    }
+}
+
+const menuState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMenu = {
+    openMenu(){
+        menuState.hidden = !menuState.hidden
+        miniCartState.hidden = true
+    }
+}
+
+const toggleClose = {
+    closeAll(){
+        miniCartState.hidden = true 
+        menuState.hidden = true
+    }
+}
